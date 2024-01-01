8eb99755012cf22d16369cafdf41ecc364157810
etoile-lune <150879303+etoile-lune@users.noreply.github.com>
2024-01-01 17:02:49 +0800

    Add files via upload

---
+++ tcpServer/mainwindow.h
@@ -0,0 +1,45 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QByteArray>
+#include <QWidget>
+#include <QDialog>
+#include <QMainWindow>
+#include <QPushButton>
+#include<QMessageBox>
+#include <QSqlTableModel>
+#include<QtSql/QSqlDatabase>
+#include<QDebug>
+#include <QTcpSocket>
+#include <QByteArray>
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    bool connectdatabase(const QString &dbName);
+public slots:
+    void newConnection();
+    void readyRead();
+    void clientDisconnected();
+private:
+    QTcpServer* m_server;
+    QTcpSocket* m_client;
+    QSqlTableModel *model;
+    QString m_receivedId;
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
